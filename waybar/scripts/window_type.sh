08f28e7d4fd3dca413c4159d4cb2a743c41e912b
Junaid <junaid96.pk@gmail.com>
2025-05-17 17:29:10 +0300

    updated waybar

---
+++ waybar/scripts/window_type.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+active=$(hyprctl activewindow -j)
+
+workspace_name=$(echo "$active" | jq -r '.workspace.name // empty')
+floating=$(echo "$active" | jq -r '.floating // empty')
+pseudo=$(echo "$active" | jq -r '.pseudo // empty')
+
+if [[ "$workspace_name" == "special:magic" ]]; then
+  echo "󱡄"
+elif [[ "$floating" == "true" ]]; then
+  echo "󰊠"
+else
+  echo ""
+fi
